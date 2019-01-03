2d3b05ac65da4bdffa6665b7e4fd674704ad1347
Hope Tambala <hopetambala@outlook.com>
2019-01-02 20:49:02 -0500

    update()

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  parseAppId: 'vBdTHqQU31IyLW5uYRDIWb8Ew8zCZGBzMqChugjr',
-  parseJavascriptKey: 'jFWiqliNRHXiH72J9kiotL8m0EuSdry1yFIiYlad',
-  parseServerUrl: 'https://parseapi.back4app.com/'
-};
