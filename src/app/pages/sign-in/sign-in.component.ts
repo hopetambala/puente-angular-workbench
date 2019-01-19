348fde695290710a76a942c512f7dcb814a3b408
Hope Tambala <hopetambala@outlook.com>
2019-01-19 00:29:12 -0500

    init(): signing page and form

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {Parse} from 'parse';
+
+import { NavbarService } from '../../providers/navbar/navbar.service';
+import { AuthService } from '../../providers/auth/auth.service';
+
+
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  password: string = '';
+  username: string = '';
 
-  constructor() { }
+  constructor(
+    private authPvdr: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private nav:NavbarService) { }
 
   ngOnInit() {
+    this.nav.hide();
+  }
+  async doSignin(){
+    //this.authPvdr.signin(this.username, this.password)
+    //  .subscribe(
+    //    success => {
+    await Parse.User.logIn(this.username, this.password);
+    this.router.navigate(['/home']);
+    // },
+    //    error => {
+            //this.alertService.error(error);
+            //this.loading = false;
+    //    });
   }
 
 }
