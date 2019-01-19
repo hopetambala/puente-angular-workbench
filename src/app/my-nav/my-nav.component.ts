68872e4acbcca28456ffde6c77aa6f0745beeb28
Hope Tambala <hopetambala@outlook.com>
2019-01-19 13:14:52 -0500

    chore(): add navbar service

--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 
+import { AuthService } from '../providers/auth/auth.service';
 import { NavbarService } from '../providers/navbar/navbar.service';
+import { async } from '@angular/core/testing';
 
 
 @Component({
@@ -10,11 +12,19 @@ import { NavbarService } from '../providers/navbar/navbar.service';
   templateUrl: './my-nav.component.html',
   styleUrls: ['./my-nav.component.scss']
 })
-export class MyNavComponent {
+export class MyNavComponent implements OnInit {
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+
+  username = this.auth.currentUser().name;
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private nav:NavbarService) {
-    
+    private nav:NavbarService,
+    private auth:AuthService) {
+
+        //this.username = String(this.auth.currentUser().name)
+      }
+        
+    ngOnInit() {
+      this.auth.authenticated()
+    }
   }
-}
