7c2a19d00aa5faf31cffa4d10d94d678c413d005
Hope Tambala <hopetambala@outlook.com>
2019-02-28 14:39:47 -0500

    refactor(): remove unneccessary comments

--- src/app/pages/first-page/first-page.component.ts
+++ src/app/pages/first-page/first-page.component.ts
@@ -22,9 +22,6 @@ export class FirstPageComponent implements OnInit {
     private nav:NavbarService,
     private data:DataExportService) { 
     this.nav.show();
-    //this.getAllObjects()
-
-    
   }
 
   async ngOnInit() {
@@ -33,170 +30,20 @@ export class FirstPageComponent implements OnInit {
   }
 
   /**
-    
- 
-  async setup() {
-    return this.query.genericQuery('SurveyData',this.organization).then((patientResults) => {
-      for (var i = 0; i < patientResults.length; i++){
-        let demographicObject = patientResults[i];
-        console.log(demographicObject)
-        
-        this.setupQuery(demographicObject)
-      }
-    });
-  }
-
-  async setupQuery(demographicObject){
-    await this.query.exactlyOneQuery('HistoryEnvironmentalHealth','client',demographicObject).then((environmentalHealthResults) =>{
-      if (environmentalHealthResults){
-          this.dictsToExport.push({
-              firstName: environmentalHealthResults.get('client').get('fname'),
-              lastName: environmentalHealthResults.get('client').get('lname'),
-              dob: environmentalHealthResults.get('client').get('dob'),
-              sex: environmentalHealthResults.get('client').get('sex'),
-              marriageStatus: environmentalHealthResults.get('client').get('marriageStatus'),
-              educationLevel: environmentalHealthResults.get('client').get('educationLevel'),
-              occupation: environmentalHealthResults.get('client').get('occupation'),
-              communityName: environmentalHealthResults.get('client').get('communityname'),
-              city: environmentalHealthResults.get('client').get('city'),
-              province: environmentalHealthResults.get('client').get('province'),
-              insuranceNumber: environmentalHealthResults.get('client').get('insuranceNumber'),
-              insuranceProvider: environmentalHealthResults.get('client').get('insuranceProvider'),
-              clinicProvider: environmentalHealthResults.get('client').get('clinicProvider'),
-              cedulaNumber: environmentalHealthResults.get('client').get('cedulaNumber'),
-              dataCollector: environmentalHealthResults.get('client').get('surveyingUser'),
-              latitude: environmentalHealthResults.get('client').get('latitude'),
-              longitude: environmentalHealthResults.get('client').get('longitude'),
-
-              yearsLivedinthecommunity: environmentalHealthResults.get('yearsLivedinthecommunity'),
-              yearsLivedinThisHouse: environmentalHealthResults.get('yearsLivedinThisHouse'),
-              medicalproblemswheredoyougo: environmentalHealthResults.get('medicalproblemswheredoyougo'),
-              dentalproblemswheredoyougo: environmentalHealthResults.get('dentalproblemswheredoyougo'),
-              numberofIndividualsLivingintheHouse: environmentalHealthResults.get('numberofIndividualsLivingintheHouse'),
-              numberofChildrenLivinginHouseUndertheAgeof5: environmentalHealthResults.get('numberofChildrenLivinginHouseUndertheAgeof5'),
-              biggestproblemofcommunity: environmentalHealthResults.get('biggestproblemofcommunity').replace(/,/g, '-'),
-              houseownership: environmentalHealthResults.get('houseownership'),
-              latrineAccess: environmentalHealthResults.get('latrineAccess'),
-              waterAccess: environmentalHealthResults.get('waterAccess'),
-              typeOfWaterDrank: environmentalHealthResults.get('typeofWaterdoyoudrink'),
-              clinicAccess: environmentalHealthResults.get('clinicAccess'),
-              trashPickUpFrequency: environmentalHealthResults.get('timesperweektrashcollected'),
-              trashLocation: environmentalHealthResults.get('wheretrashleftbetweenpickups'),
-              conditionOfFloor: environmentalHealthResults.get('conditionoFloorinyourhouse'),
-              conditionOfRoof: environmentalHealthResults.get('conditionoRoofinyourhouse'),
-              organizationWhoOwnsData: environmentalHealthResults.get('client').get('surveyingOrganization')
-            
-        })
-      }
-      else {
-        this.dictsToExport.push({
-          //patient:{
-            firstName: demographicObject.get('fname'),
-            lastName: demographicObject.get('lname'),
-            dob: demographicObject.get('dob'),
-            sex: demographicObject.get('sex'),
-            marriageStatus: demographicObject.get('marriageStatus'),
-            educationLevel: demographicObject.get('educationLevel'),
-            occupation: demographicObject.get('occupation'),
-            communityName: demographicObject.get('communityname'),
-            city: demographicObject.get('city'),
-            province: demographicObject.get('province'),
-            insuranceNumber: demographicObject.get('insuranceNumber'),
-            insuranceProvider: demographicObject.get('insuranceProvider'),
-            clinicProvider: demographicObject.get('clinicProvider'),
-            cedulaNumber: demographicObject.get('cedulaNumber'),
-            dataCollector: demographicObject.get('surveyingUser'),
-            latitude: demographicObject.get('latitude'),
-            longitude: demographicObject.get('longitude'),
-
-            yearsLivedinthecommunity: null,
-            yearsLivedinThisHouse: null,
-            medicalproblemswheredoyougo: null,
-            dentalproblemswheredoyougo: null,
-            numberofIndividualsLivingintheHouse:null,
-            numberofChildrenLivinginHouseUndertheAgeof5:null,
-            biggestproblemofcommunity:null,
-            houseownership:null,
-            latrineAccess: null,
-            waterAccess: null,
-            typeOfWaterDrank:null,
-            clinicAccess: null,
-            trashPickUpFrequency: null,
-            trashLocation: null,
-            conditionOfFloor: null,
-            conditionOfRoof: null,
-            organizationWhoOwnsData: null
-              
-        });
-      }
-    })
-  }
-
-  async getAllObjects() {
-    await this.setup();
-    console.log(this.dictsToExport)
-  }
-  
-  convertArrayOfObjectsToCSV(args) {  
-    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
-
-    data = args.data || null;
-    if (data == null || !data.length) {
-        return null;
-    }
-
-    columnDelimiter = args.columnDelimiter || ',';
-    //columnDelimiter = args.columnDelimiter || '\t';
-    lineDelimiter = args.lineDelimiter || '\n';
-
-    keys = Object.keys(data[0]);
-
-    result = '';
-    result += keys.join(columnDelimiter);
-    result += lineDelimiter;
-
-    data.forEach(function(item) {
-        ctr = 0;
-        keys.forEach(function(key) {
-            if (ctr > 0) result += columnDelimiter;
-
-            result += item[key];
-            ctr++;
-        });
-        result += lineDelimiter;
-    });
-
-    return result;
-  }
-
-  async downloadCSV(args) {  
-    var data, filename, link;
-    var csv = await this.convertArrayOfObjectsToCSV({
-        data: await this.dictsToExport
-    });
-    if (csv == null) return;
-
-    filename = args.filename || 'export.csv';
-
-    if (!csv.match(/^data:text\/csv/i)) {
-        csv = 'data:text/csv;charset=utf-8,' + csv;
-    }
-    data = encodeURI(csv);
-
-    link = document.createElement('a');
-    link.setAttribute('href', data);
-    link.setAttribute('download', filename);
-    link.click();
-  }
-   */
-
+    * @example
+    * Returns an array of objects of the desired backend
+    * testSetup('EvalMedical')
+    *
+    * @param {string} Backend Model  
+    * @returns array of objects
+  */
   async testSetup(form:string){
     await this.data.setup(this.organization,form).then((results)=>{
     //this.data.setup(this.organization,'EvalMedical').then((results)=>{
     //this.data.setup(this.organization,'EnvForm').then((results)=>{
     //this.data.setup(this.organization,'Vitals').then((results)=>{
       this.dictsToExport = results
-    })
+  })
    
     
   }
